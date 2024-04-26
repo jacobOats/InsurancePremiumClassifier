fd295a9fd8d3c2d20dc5e2be910ea8e3bd9165f4
jacobOats <97692358+jacobOats@users.noreply.github.com>
2024-04-26 16:52:42 -0400

    Delete test/java/com/group1/ipc directory

--- test/java/com/group1/ipc/unit_tests/controllers/EmployeeControllerTests.java
+++
@@ -1,5 +0,0 @@
-package com.group1.ipc.unit_tests.controllers;
-
-public class EmployeeControllerTests {
-
-}
--- test/java/com/group1/ipc/unit_tests/services/ClaimServiceTests.java
+++
@@ -1,5 +0,0 @@
-package com.group1.ipc.unit_tests.services;
-
-public class ClaimServiceTests {
-
-}
