05df371f636521ee912108587fab12b217e96ec1
jacobOats <97692358+jacobOats@users.noreply.github.com>
2024-04-26 16:52:33 -0400

    Delete main directory

--- main/java/com/group1/ipc/repositories/IClaimRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group1.ipc.repositories;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import com.group1.ipc.entities.Claim;
-
-public interface IClaimRepository extends ListCrudRepository<Claim, Integer> {
-
-}
--- main/java/com/group1/ipc/repositories/IOrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group1.ipc.repositories;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import com.group1.ipc.entities.Organization;
-
-public interface IOrganizationRepository extends ListCrudRepository<Organization, Integer> {
-
-}
