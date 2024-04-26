a448c0f1cb1f22b3cabf36e77b334571f503e674
jacobOats <97692358+jacobOats@users.noreply.github.com>
2024-04-26 16:43:00 -0400

    Add files via upload

---
+++ test/java/com/group1/ipc/IpcApplicationTests.java
@@ -0,0 +1,15 @@
+package com.group1.ipc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class IpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
